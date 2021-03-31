2e2441e0b50502b3f1f77e68fb5302500340f222
Pineapple Season <emsthai21@gmail.com>
2021-03-31 02:48:07 -0700

    record search history to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var searchElement = document.querySelector("#city-search");
 var userInput = document.querySelector("#cityInput");
 var historyContainer = document.querySelector("#search-history");
 var history = JSON.parse(localStorage.getItem("history")) || [];
+var apiKey = "3273be5ddae13e1b574c10140ba3a466";
 
 var searchFunction = function (event) {
     var search = userInput.value.trim(); 
@@ -18,5 +19,19 @@ var searchFunction = function (event) {
 
 var forecastInfo = function (search) {
     // Add a feature to ask user if they prefer metric or imperial units
-    var searchURL = "api.openweathermap.org/data/2.5/weather?q=" + search + 
+    var searchURL = "api.openweathermap.org/data/2.5/weather?q=" + search
+        + "&units=imperial&appid=" + apiKey;
+    fetch(searchURL)
+        .then(response => response.json())
+        .then(function (weatherInfo) {
+            history.unshift(weatherInfo.name);
+            localStorage.setItem("Search History", json.stringify(history));
+            showHistory();
+        })
+
+    
+}
+
+function showHistory() {
+
 }
