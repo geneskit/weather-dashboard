8e3965a395f025840294e4c1407ae0c070c14a19
Pineapple Season <emsthai21@gmail.com>
2021-03-31 04:24:28 -0700

    begin setting up five day summary

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,47 @@ var forecastInfo = function (search) {
             showHistory();
         })
 
+    var searchLat = weatherInfo.coord.lat;
+    var searchLon = weatherInfo.coord.lat;
+    var uvSearchURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + searchLat 
+        + "&lon=" + searchLon + "&exclude=minutely,hourly&appid=" + apiKey;
+    fetch(uvSearchURL)
+        .then(response => response.json)
+        .then(function (weatherInfo) {
+            var uvI = weatherInfo.value;
+            if (uvI <= 2) {
+                $(".badge-success").text("UV Index: " + uvI);
+                $(".badge-success").show();
+                $(".badge-warning").hide();
+                $(".badge-danger").hide(); 
+            }
+            else {
+                if (uvI > 2 && uvI <=7) {
+                    $(".badge-warning").text("UV Index: " + uvI);
+                    $(".badge-success").hide();
+                    $(".badge-warning").show();
+                    $(".badge-danger").hide(); 
+                }
+                else {
+                    $(".badge-danger").text("UV Index: " + uvI);
+                    $(".badge-success").hide();
+                    $(".badge-warning").hide();
+                    $(".badge-danger").show(); 
+                }
+            }
+        })
+    var now = moment().format("M/D/YYYY");
+
+    $(".badge").display = "none";
+
+    $(".cityInput").html("<h1>" + weatherInfo.name + " (" + now + ")" + "</h1>");
+    $(".icon").attr("src", "http://openweathermap.org/img/w/" + weatherInfo.weather[0].icon
+        + ".png");
+    $(".temperatureResult").text("Temperature: " + Math.floor(weatherInfo.current.temp) + "° F")
+    $(".humidityResult").text("Humidity: " + weatherInfo.current.humidity + "%");
+    $(".windResult").text("Wind Speed: " + weatherInfo.current.wind_speed + " MPH")
+
+    var fiveDayURL = "api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=" + apiKey;
     
 }
 
